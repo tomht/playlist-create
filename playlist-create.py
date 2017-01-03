e9d977977da78f88f891e8e82f12288296eaabba
tomht <tom.hewlett.taylor@googlemail.com>
2017-01-03 18:41:18 +0000

    Fix line spacing.

--- playlist-create.py
+++ playlist-create.py
@@ -15,6 +15,7 @@ def has_supported_extension(file):
     extension = file.split('.')[-1]
     return extension in supported_extensions
 
+
 root_directory = "/Users/tom/Music"
 
 artist_directories = get_subdirectories(root_directory)
@@ -25,7 +26,8 @@ for artist_directory in artist_directories:
         path_parts = album_directory.split('/')
         album_artist = path_parts[-2]
         album_title = path_parts[-1]
-        track_files = [os.path.join(album_directory, f) for f in os.listdir(album_directory) if has_supported_extension(f)]
+        track_files = [os.path.join(album_directory, f) for f in os.listdir(album_directory) if
+                       has_supported_extension(f)]
         track_numbers = []
         for track_file in track_files:
             track = EasyID3(track_file)
@@ -36,6 +38,3 @@ for artist_directory in artist_directories:
         with open(os.path.join(album_directory, playlist_filename), 'w') as playlist_file:
             for track in track_numbers:
                 playlist_file.write(track[0] + "\n")
-
-
-
